4577ede20a17ee4afaf3759f9b2e64ab8609fcd7
Admin <136904507@qq.com>
2021-02-15 17:54:06 +0800

    2021年02月15日 17:54:05

--- core/library/think/App.php
+++ core/library/think/App.php
@@ -1,13 +1,5 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
+
 
 namespace think;
 
@@ -18,7 +10,6 @@ use think\exception\RouteNotFoundException;
 
 /**
  * App 应用管理
- * @author liu21st <liu21st@gmail.com>
  */
 class App
 {

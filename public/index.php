35859ec374dd5220dd78f2bd097bd0e18c3caa39
Admin <136904507@qq.com>
2022-06-27 12:58:41 +0800

    6.0

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2019 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
@@ -10,10 +10,15 @@
 // +----------------------------------------------------------------------
 
 // [ 应用入口文件 ]
+namespace think;
 
-// 定义应用目录
-define('APP_PATH', __DIR__ . '/../app/');
-// 加载框架引导文件
-require __DIR__ . '/../core/start.php';
-// 自动生成对应的模块 生成模块已经被修复
-//\think\Build::module('home');
+require __DIR__ . '/../vendor/autoload.php';
+
+// 执行HTTP应用并响应
+$http = (new App())->http;
+
+$response = $http->run();
+
+$response->send();
+
+$http->end($response);

05c8dd32566a857503311a397776dc85a2668f37
YanHe2018 <136904507@qq.com>
2023-05-04 18:31:30 +0800

    2023-05-04 18:31:30
    2023.05.04

---
+++ core/library/acloud.php
@@ -0,0 +1,14 @@
+<?php
+//define('WEKIT_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR);
+//define('WEKIT_VERSION', '0.2');
+
+Wind::import('SRC:phpwind');
+
+/**
+ * @author YanHe
+ * @version $Id: admin.php 7493 2012-04-06 10:22:42Z YanHe $
+ * @package wekit
+ */
+class acloud extends phpwind {
+	
+}
---
+++ core/library/admin.php
@@ -0,0 +1,12 @@
+<?php
+
+Wind::import('SRC:phpwind');
+
+/**
+ * @author YanHe
+ * @version $Id: admin.php 18202 2012-09-11 13:55:07Z liusanbian $
+ * @package wekit
+ */
+class admin extends phpwind {
+	
+}

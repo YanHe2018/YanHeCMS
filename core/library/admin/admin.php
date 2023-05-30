84fac632a88a9c12d4e5f865b0dd81ad7f1884a6
YanHe2018 <136904507@qq.com>
2023-05-30 10:01:34 +0800

    2023.05.30
    2023-05-30 10:01:32

--- core/library/admin/admin.php
+++ core/library/admin/admin.php
@@ -1,1 +1,12 @@
-test
+<?php
+
+Wind::import('SRC:phpwind');
+
+/**
+ * @author YanHe
+ * @version $Id: admin.php 18202 2012-09-11 13:55:07Z liusanbian $
+ * @package wekit
+ */
+class admin extends phpwind {
+	
+}
